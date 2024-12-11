511b95fba62d2525092ed1675186b2f6ed0d3556
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-12-10 20:13:35 -0500

    Update Loom, Gradle, Parchment

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import net.fabricmc.loom.api.LoomGradleExtensionAPI
 
 plugins {
     id("architectury-plugin") version "3.4-SNAPSHOT"
-    id("dev.architectury.loom") version "1.7-SNAPSHOT" apply false
+    id("dev.architectury.loom") version "1.9-SNAPSHOT" apply false
     id("com.gradleup.shadow") version "8.3.5" apply false
     java
     idea
@@ -43,7 +43,7 @@ subprojects {
         "minecraft"("com.mojang:minecraft:$minecraftVersion")
         "mappings"(loom.layered{
             officialMojangMappings()
-            parchment("org.parchmentmc.data:parchment-1.21:${project.properties["parchment"]}@zip")
+            parchment("org.parchmentmc.data:parchment-$minecraftVersion:${project.properties["parchment"]}@zip")
         })
 
         compileOnly("org.jetbrains:annotations:26.0.1")
